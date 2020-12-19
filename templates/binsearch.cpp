f96c42d89d5989e2dcc570b9fd7f4ec7f09c761e
daniel-sudz <daniel@sudzilouski.com>
2020-12-19 14:37:46 -0800

    Create binsearch.cpp

---
+++ templates/binsearch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid = (low + high) / 2;
+    while (low <= high) {
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] > target) {
+            high = mid - 1;
+        }
+        else if (nums[mid] < target) {
+            low = mid + 1;
+        }
+        mid = (low + high) / 2;
+    }
+    return -1;
+    }
+};
