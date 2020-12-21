0fa091e950aff23da84a18a3fda7b2d5b1a98618
daniel-sudz <daniel@sudzilouski.com>
2020-12-20 17:51:56 -0800

    Create skeleton.cpp

---
+++ templates/skeleton.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using ll = long long;
+
+int main() {
+    ofstream fout("problem.out");
+    ifstream fin("problem.in");
+
+    int N;
+    int M;
+    fin >> N >> M;
+}
