50dbfe0259017c74514ad9f9b38a13e8973fa5d8
daniel-sudz <daniel@sudzilouski.com>
2020-12-21 22:27:32 -0800

    Create skeletonv2.cpp

---
+++ templates/skeletonv2.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <iostream>
+
+using ll = long long;
+
+// competition input/output
+#define read cin 
+#define write cout
+
+// debug input/output  
+ofstream fout("problem.out");
+ifstream fin("problem.in");
+
+//#define read fin
+//#define write fout
+
+int main() {
+
+    int N;
+    read >> N; 
+}
