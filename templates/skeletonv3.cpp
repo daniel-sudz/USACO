4985136c3a6c9269cb18e69ca2d3de194c15f780
daniel-sudz <daniel@sudzilouski.com>
2020-12-28 21:36:26 -0800

    Update skeletonv3.cpp

--- templates/skeletonv3.cpp
+++ templates/skeletonv3.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
+#define pb push_back
+#define read fin
+#define write fout
+#pragma warning(disable:6385)
+const string prob = "example"; 
 using namespace std;
 using ll = long long;
 using vi = vector<int>;
+ifstream fin(prob + ".in");
+ofstream fout(prob + ".out");
 
 
 int main() {
-    ifstream fin("debug.in");
 }
