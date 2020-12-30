896b12d2f220938b39a3944f848fb833d769a74e
daniel-sudz <daniel@sudzilouski.com>
2020-12-30 14:28:04 -0800

    Update skeletonv3.cpp

--- templates/skeletonv3.cpp
+++ templates/skeletonv3.cpp
@@ -3,12 +3,11 @@
 #define read fin
 #define write fout
 #pragma warning(disable:6385)
-const string prob = "example"; 
 using namespace std;
 using ll = long long;
 using vi = vector<int>;
-ifstream fin(prob + ".in");
-ofstream fout(prob + ".out");
+ifstream fin(".in");
+ofstream fout(".out");
 
 
 int main() {
